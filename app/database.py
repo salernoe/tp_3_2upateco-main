c21bf1b7569b28acd54bcd235a93c8ace426b3ce
salernoe <ernestoasalerno@yahoo.com.ar>
2023-09-20 22:38:07 -0300

    1
    1

--- app/database.py
+++ app/database.py
@@ -14,14 +14,32 @@ class DatabaseConnection:
         return cls._connection
     
     @classmethod
-    def fetch_one(cls, query, params=None):
-        cursor = cls.get_connection().cursor()
-        cursor.execute(query, params)
-        return cursor.fetchone()
+    def set_config(cls, config):
+        cls._config = config
     
     @classmethod
-    def execute_query(cls, query, params=None):
+    def execute_query(cls, query, database_name=None, params=None):
         cursor = cls.get_connection().cursor()
         cursor.execute(query, params)
         cls._connection.commit()
-        return cursor()
+        
+        return cursor
+    
+    @classmethod
+    def fetch_all(cls, query, database_name=None, params=None):
+        cursor = cls.get_connection().cursor()
+        cursor.execute(query, params)
+        return cursor.fetchall()
+    
+    @classmethod
+    def fetch_one(cls, query, database_name=None, params=None):
+        cursor = cls.get_connection().cursor()
+        cursor.execute(query, params)
+        
+        return cursor.fetchone()
+    
+    @classmethod
+    def close_connection(cls):
+        if cls._connection is not None:
+            cls._connection.close()
+            cls._connection = None
